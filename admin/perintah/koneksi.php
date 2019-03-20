573dd17e1693edff791400474f55a2ac9d6b10b4
CodeBengs <47754955+CodeBengs@users.noreply.github.com>
2019-03-20 15:32:46 +0700

    Add files via upload

---
+++ admin/perintah/koneksi.php
@@ -0,0 +1,7 @@
+<?php 
+$conn = mysqli_connect("localhost","root","","sepatusport");
+
+// $koneksi =new mysqli("localhost","root","","sepatusport");
+	$koneksi = new mysqli("localhost","root","","sepatusport");
+
+ ?>
